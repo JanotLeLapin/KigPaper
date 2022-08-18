f48fff42defbdee82c4a28eaf0c6c2cf470ef524
Joseph <joseph300905@gmail.com>
2022-08-18 13:38:09 +0200

    Setup Gradle

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install && ./paperclip.sh
+git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && chmod +x ./gradlew && ./gradlew shadowJar && cp ./PaperSpigot-Server/build/libs/paperspigot-server-all.jar ./paper.jar
+
